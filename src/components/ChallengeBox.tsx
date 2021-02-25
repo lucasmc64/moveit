3993c0f971436b3965ef11a2d40a6a3c0ddc57fe
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2021-02-25 15:25:48 -0300

    Day 4

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,23 @@
 import { useContext } from "react"
+
 import { ChallengesContext } from "../contexts/ChallengesContext"
+import { CountdownContext } from "../contexts/CountdownContext"
 
 import styles from "../styles/components/ChallengeBox.module.css"
 
 export function ChallengeBox() {
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext)
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext)
+    const { resetCountdown } = useContext(CountdownContext)
+
+    function handleChallengeSucceeded() {
+        completeChallenge()
+        resetCountdown()
+    }
+
+    function handleChallengeFailed() {
+        resetChallenge()
+        resetCountdown()
+    }
 
     return (
         <div className={ styles.challengeBoxContainer }>
@@ -27,8 +40,8 @@ export function ChallengeBox() {
                     </main>
 
                     <footer>
-                        <button type="button" className={ styles.challengeFailedButton } onClick={ resetChallenge }>Falhei</button>
-                        <button type="button" className={ styles.challengeSucceededButton }>Completei</button>
+                        <button type="button" className={ styles.challengeFailedButton } onClick={ handleChallengeFailed }>Falhei</button>
+                        <button type="button" className={ styles.challengeSucceededButton }  onClick={ handleChallengeSucceeded }>Completei</button>
                     </footer>
                 </div>
             ) : (
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+
+import { ChallengesContext } from "../contexts/ChallengesContext";
+
 import styles from "../styles/components/Profile.module.css"
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={ styles.profileContainer }>
             <img src="https://github.com/lucasmc64.png" alt="Lucas Coutinho"/>
@@ -10,7 +16,7 @@ export function Profile() {
                 </strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level { level }
                 </p>
             </div>
         </div>
